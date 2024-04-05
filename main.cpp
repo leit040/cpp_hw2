9cb3160a116e589283979d36bef95f876135600a
leit <leit.2040@gmail.com>
2024-04-05 20:53:11 +0300

    std::swap

--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@ using namespace std;
 void revertArray(int *arr, int size)
 {
     for (int i = 0; i < size / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[size - i - 1];
-        arr[size - i - 1] = temp;
+       std::swap(arr[i], arr[size - i - 1]);
+       //а от оце взагалі цікаво
     }
 }
 
 int main() {
-    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+    int arr[]{ 1,2,3,4,5,6,7,8,9,10 ,11};
     revertArray(arr, sizeof(arr) / sizeof(arr[0]));
     std::cout << "[ ";
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {

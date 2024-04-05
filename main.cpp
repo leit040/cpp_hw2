9d528e261f1d8713fa98ddd88ebc7781116c48ff
leit <leit.2040@gmail.com>
2024-04-05 20:15:10 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-#define true false //))!!
+
+using namespace std;
+
+int calc(int first,int second, char operand){
+    switch (operand){
+        case '+':
+            return first + second;
+        case '-':
+            return first - second;
+        case '*':
+            return first * second;
+        case '/':
+            if(second){
+                return first / second;
+            }
+            std::cout << "Warning: Division by zero !!!"  << std::endl;
+            break;
+        default:
+            std::cout << "Warning: Please choose only + - * /"  << std::endl;
+            break;
+    }
+    return 0;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << "Sum: " << calc(3,5,'+') << std::endl;
+    std::cout << "Subtraction: " << calc(3,5,'-') << std::endl;
+    std::cout << "Multiplying: " << calc(3,5,'*') << std::endl;
+    std::cout << "Division: " << calc(3,5,'/') << std::endl;
+    std::cout << "Non correct input: " << calc(3,5,'!') << std::endl;
+    std::cout << "Division by zero: " << calc(3,0,'/') << std::endl;
+
+
     return 0;
 }
